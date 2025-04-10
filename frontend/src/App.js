e05203a08524f4ea4c0869f7d90ebfdf55bcc125
pbcool <pranavcool2005@gmail.com>
2025-04-10 21:30:11 +0530

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Result from './pages/Result';
+import Home from './pages/Home';
+import ComplaintForm from './pages/ComplaintForm';
+import { useState } from 'react';
+import RefrshHandler from './RefrshHandler';
+import Navbar from './components/Navbar';
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
+  return (
+    <div className="App">
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
+        <Route path='/complaintform' element={<ComplaintForm />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
